c289411b858b20c17d02227d293184d7ab7aa066
benjaminle9x <54958544+benjaminle9x@users.noreply.github.com>
2019-12-01 18:29:26 -0500

    getCount() added

--- app/src/main/java/com/example/simpleadapterv1/MainActivity.java
+++ app/src/main/java/com/example/simpleadapterv1/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.simpleadapterv1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Initialize view's
+    ListView simpleListView;
+    TextView mTView;
+    View v;
+    Button bt;
+
+    //Get images array
+    String[] animalName={"Lion","Tiger","Monkey","Dog","Cat","Elephant"};
+    int[] animalImages={R.drawable.lion,R.drawable.tiger,R.drawable.monkey,R.drawable.dog,R.drawable.cat,R.drawable.elephant};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTView = (TextView)findViewById(R.id.tView);
+        simpleListView=(ListView)findViewById(R.id.simpleListView);
+        bt = (Button)findViewById(R.id.button);
+
+        ArrayList<HashMap<String,String>> arrayList=new ArrayList<>();
+        for (int i=0;i<animalName.length;i++)
+        {
+            HashMap<String,String> hashMap=new HashMap<>(); //create a hashmap to store the data in key value pair
+            hashMap.put("name",animalName[i]);
+            hashMap.put("image",animalImages[i]+"");
+            arrayList.add(hashMap);//add the hashmap into arrayList
+        }
+        String[] from={"name","image"};//string array
+        int[] to={R.id.textView,R.id.imageView};//int array of views id's
+        final SimpleAdapter simpleAdapter=new SimpleAdapter(this,arrayList,R.layout.activity_list_view_items,from,to);//Create object and set the parameters for simpleAdapter
+        simpleListView.setAdapter(simpleAdapter);//sets the adapter for listView
+
+        //perform listView item click event
+        simpleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),animalName[i],Toast.LENGTH_SHORT).show();//show the selected image in toast according to position);
+            }
+        });
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int i;
+                i = simpleAdapter.getCount(); //Count the number of items represented by this Adapter
+                mTView.setText("Number of items: " + i);
+            }
+        });
+
     }
+
 }
